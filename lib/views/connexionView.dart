7d0831baae5e3b0e3760cf2fd100c07a5f6b517f
Maxime Braud <maxime.braud86@gmail.com>
2023-07-17 19:09:45 +0200

    Création de la page connexion

---
+++ lib/views/connexionView.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class ConnexionView extends StatelessWidget {
+  const ConnexionView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Diodon"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Text('Connexion', style: Theme.of(context).textTheme.headline2),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: const [
+                CircleAvatar(
+                  minRadius: 60,
+                  child: Text('Aurélien'),
+                ),
+                CircleAvatar(
+                  minRadius: 60,
+                  child: Text('Patick'),
+                ),
+                CircleAvatar(
+                  minRadius: 60,
+                  child: Text('Eric'),
+                ),
+              ],
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, "/register");
+                },
+                child: const Text("Ajouter un utilisateur"))
+          ],
+        ),
+      ),
+    );
+  }
+}
